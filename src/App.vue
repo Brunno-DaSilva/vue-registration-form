c7ee2165a46ce41f1f10f81b811c4ba47deb0caa
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-09-29 15:49:02 -0500

    added gradient bg and box-shadow

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@
   body {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     overflow: hidden !important;
+    background-image: linear-gradient(to right, #ffecd2 0%, #fcb69f 100%);
   }
   #app {
     width: 100vw;
@@ -41,11 +42,13 @@
     justify-content: center;
   }
   .app {
-    border: 1px solid tomato;
+    width: 80%;
+    min-height: 80%;
     margin: 3rem;
+    /* box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); */
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   }
   .app__container {
     min-height: 80%;
-    background: #2c3e50;
   }
 </style>
